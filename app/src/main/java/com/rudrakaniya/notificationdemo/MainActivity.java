69634909636ede987997be0592e4a550efd0e349
Rudra <rudra.kaniya.rk@gmail.com>
2020-12-16 23:00:42 +0530

    adding a single notification firstly

--- app/src/main/java/com/rudrakaniya/notificationdemo/MainActivity.java
+++ app/src/main/java/com/rudrakaniya/notificationdemo/MainActivity.java
@@ -8,9 +8,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.icu.util.Calendar;
 import android.os.Bundle;
-import android.util.Log;
-
-import com.rudrakaniya.notificationdemo.reminder.AlarmScheduler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,18 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-//
-//        Intent notificationIntent = new Intent(this, AlarmReceiver.class);
-//        PendingIntent broadcast = PendingIntent.getBroadcast(this, 100, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-//
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Intent notificationIntent = new Intent(this, AlarmReceiver.class);
+        PendingIntent broadcast = PendingIntent.getBroadcast(this, 100, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.SECOND, 2);
-//        alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), broadcast);
+        cal.add(Calendar.SECOND, 5);
+        cal.set(Calendar.DATE, 16);
+//        cal.set(Calendar.)
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), broadcast);
 
 
-        new AlarmScheduler().scheduleAlarm(getApplicationContext(), cal.getTimeInMillis());
-        Log.d("TAG", "onHandleWork: " + "WE are here!!!!    inside main activity");
 
     }
 }
